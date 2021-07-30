e62fcdec1c1590082ff363c396251f64eec18788
Josemar Luedke <josemarluedke@gmail.com>
2021-07-29 20:45:02 -0700

    Setup webpack loader using graphql-tag

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import query from '../gql/query.graphql';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  model() {
+    console.log(query);
+
+    return query;
+  }
+}
